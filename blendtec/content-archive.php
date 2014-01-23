61b0ba50b14e6e36c32deb49d424b9bd0f0a9eb7
Lindley White <lindleywhite@gmail.com>
2014-01-23 16:06:40 -0600

    Change classes for a grid approach

--- blendtec/content-archive.php
+++ blendtec/content-archive.php
@@ -2,18 +2,14 @@
   <!-- displays thumbnail if it exists -->
   <?php if ( has_post_thumbnail()) : ?>
     <div class="post_featured_img">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail( 'full'); ?></a>
+        <div class="image_wrap">
+          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail( 'full'); ?></a>
+        </div>
     </div>
   <?php endif; ?>
-  <div class="post_details">
-      <div class="date_posted">
-     		<div class="month_posted"><?php the_time('M') ?></div>
-        <div class="day_posted"><?php the_time('j') ?></div>
-      </div>
-    <div class="author">Posted By: <?php the_author() ?></div>
-  </div>
   <div class="the_post">
     <h2 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+    <div class="author"><i class="icon-user"></i><?php the_author() ?></div><div class='date'><i class="icon-clock"></i><?php the_time("d F, Y") ?></div>
     <p><?php the_excerpt();?></p>
   </div>
 </div><!-- single-excerpt -->
