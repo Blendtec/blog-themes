faa31d455724729deba7df5f5046c482740d7eb5
Lindley White <lindleywhite@gmail.com>
2014-01-23 16:13:45 -0600

    Change classes for grid

--- blendtec/front-page.php
+++ blendtec/front-page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
    
-<div id="main">
+<div id="main" class="container">
   <div id="content" class="row">
 
-        <div id="excerpts" class="col-md-9">
+        <div id="excerpts" class="col-md-9 col-sm-9 col-xs-12">
          <?php while (have_posts()) : the_post(); ?>
               <?php get_template_part( 'content', 'archive' ); ?>
           <?php endwhile; ?>
