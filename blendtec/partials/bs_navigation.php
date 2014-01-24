11b9b4f673922d0ddfd5667cfb058507b4c3d04e
Lindley White <lindleywhite@gmail.com>
2014-01-24 14:56:15 -0600

    Have navigation working on mobile.. somewhat

---
+++ blendtec/partials/bs_navigation.php
@@ -0,0 +1,56 @@
+<div class="navbar-container">
+	<div class="navbar navbar-default navbar-main" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navbar-collapse">
+			     <span class="sr-only">Toggle navigation</span>
+			     <span class="icon-bar"></span>
+			     <span class="icon-bar"></span>
+			     <span class="icon-bar"></span>
+			   </button>
+			<a href="/" class="brand" title="Blendtec"><span>Blendtec</span></a>
+		</div>
+		<div class="collapse navbar-collapse main-navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="navlink navfirst">
+						<a href="<?php echo blendtec_url(); ?>/"><span>for home</span></a>
+						<ul>
+							<li class="firstsub">
+								<a href="<?php echo blendtec_url(); ?>/recipes" title="Recipes"><span>Recipes</span></a>										</li>
+							<li>
+								<a href="<?php echo blendtec_url(); ?>/blog" title="Blog"><span>Blog</span></a>										</li>
+							<li>
+								<a href="<?php echo blendtec_url(); ?>/products/category/blenders" title="Blenders"><span>Blenders</span></a>										</li>
+							<li>
+								<a href="<?php echo blendtec_url(); ?>/products/category/jars" title="Jars"><span>Jars</span></a>										</li>
+							<li>
+								<a href="<?php echo blendtec_url(); ?>/products/category/mills_and_mixers" title="Mixers &amp; Mills"><span>Mixers &amp; Mills</span></a>										</li>
+							<li class="lastsub">
+								<a href="<?php echo blendtec_url(); ?>/products/category/accessories" title="Accessories"><span>Accessories</span></a>										</li>
+						</ul>
+					</li>
+					<div class="menutab firsttab"></div>
+					<li class="navlink navmiddle">
+						<a href="<?php echo blendtec_url(); ?>/commercial/" title="for business"><span>for business</span></a>
+					</li>
+					<div class="menutab lasttab"></div>
+					<li class="navlink navlast">
+						<a href="<?php echo blendtec_url(); ?>/products/registration" title="Register Product"><span>registration</span></a>
+					</li>					
+				</ul>
+		</div>
+		
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="subbar-container">
+		<ul id="sublist">
+			<li><a href="tel:18002536383">1.800.BLENDTEC</a>
+			<!--<li><a href="#">chat</a></li>
+			<li><a href="#">login</a></li>-->
+			</li><li>
+				<a href="<?php echo blendtec_url(); ?>/company/contact" title="Contact Us">contact us</a>			</li>
+			<li>
+				<a href="<?php echo blendtec_url(); ?>/checkout" title="My Cart">My Cart</a>			</li>
+		</ul>
+	</div>
+</div>
