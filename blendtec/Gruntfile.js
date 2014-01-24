e8ab64bb1ae9dcfab66a202a51bde60507f89d42
Lindley White <lindleywhite@gmail.com>
2014-01-23 19:37:44 -0600

    Added jshint, concatenation, and minification of JS to Grunt build

--- blendtec/Gruntfile.js
+++ blendtec/Gruntfile.js
@@ -3,33 +3,78 @@ module.exports = function(grunt) {
 
   // Force use of Unix newlines
   grunt.util.linefeed = '\n';
-
-  RegExp.quote = require('regexp-quote')
-  var btoa = require('btoa')
+  RegExp.quote = require('regexp-quote');
+  var btoa = require('btoa');
   // Project configuration.
   grunt.initConfig({
-  	sass: {
+    sass: {
       development: {
         options: {
           style: 'expanded'
         },
         files: {
           'css/blendtec.css' : 'sass/reduced_blendtec.scss',
-          'style.css' : 'sass/main.scss' 
+          'style.css' : 'sass/main.scss'
         }     
       }       
     },
+    clean: {
+      css: ['css/blendtec.css', 'style.css'],
+      js: ['js/src/scripts.js', 'js/src/scripts.min.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      all: ['js/*.js',
+            '!js/scripts.js',
+            '!js/scripts.min.js'
+      ]
+    },
+    concat: {
+      options: {
+        stripBanners: true,
+        separator: ';'
+      },
+      dist: {
+        src: [
+          'bower_components/bootstrap-sass/dist/js/bootstrap.js'          
+        ],
+        dest: 'js/src/scripts.js'
+      }
+    },
+    uglify: {
+      options: {
+        report: 'min'
+      },
+      bootstrap: {
+        src: ['<%= concat.dist.dest %>'],
+        dest: 'js/src/scripts.min.js'
+      }
+    },
     watch: {
-    	sass: {
-    		files: ['sass/*.scss', 'sass/modules/_*.scss'],
-    		tasks: ['sass']
-    	}
+      sass: {
+        files: ['sass/*.scss', 'sass/modules/_*.scss'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['js/*.js', 'Gruntfile.js'],
+        tasks: ['concat', 'uglify']
+      }
     }
   });
-
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('js', ['jshint','concat', 'uglify']);
+  grunt.registerTask('clean-all', ['clean', 'sass', 'js']);
 
 };
