0192cc1a3cb493e48778f31ae3302c4d536b2e68
Guy Warner <gwarner@blendtec.com>
2014-03-13 13:33:13 -0600

    Fix the weird

--- blendtec/404.php
+++ blendtec/404.php
@@ -1,18 +1,11 @@
 <?php get_header(); ?>
-   
-<div id="main">
-  <div id="content">
-    
-   
-        <h2 class="center">Error 404 - Not Found</h2>
-    
-      <?php get_sidebar(); ?>
-    
-      </div>
+<div id="wrapper">
+  <div id="main" class="container">
+     <div id="content" class="row">
+ 	    <h2 class="center">Error 404 - Not Found</h2>
+        <?php get_sidebar(); ?>
+     </div>
   </div>
-
-
-
-<div id="delimiter"></div>
-
+  <div id="delimiter"></div>
+</div>
 <?php get_footer(); ?>
